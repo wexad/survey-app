5ea8346548e2c500048d5770bb7da683322705b5
wexad <sherzodchoriyev747@gmail.com>
2025-05-02 12:21:16 +0500

    finished migrations and models

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,10 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    public function surveys()
+    {
+        return $this->hasMany(Survey::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
