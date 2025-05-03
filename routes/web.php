eca84dad73977a2db2934c55fde40300783dac6f
wexad <sherzodchoriyev747@gmail.com>
2025-05-03 09:36:17 +0500

    edited web.php

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', [SurveyController::class, 'index'])->name('survey.index');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('surveys', SurveyController::class)->middleware('auth');
-    Route::get('/survey/add-question', [SurveyController::class, 'addQuestion'])->name('surveys.add-question');
-    Route::get('/surveys/create', [SurveyController::class, 'create'])->name('surveys.create');
-    Route::post('/surveys/store', [SurveyController::class, 'store'])->name('surveys.store');
-    Route::get('/survey/{surveyId}/add-question', [QuestionController::class, 'addQuestion'])->name('surveys.add-question');
-    Route::post('/surveys', [SurveyController::class, 'store'])->name('surveys.store');
-    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
-    Route::get('/form/{surveyId}', [SurveyController::class, 'show'])->name('surveys.show');
-    Route::get('/form/{surveyId}/statistic', [SurveyController::class, 'showStatistics'])->name('surveys.statistic');
-});
-
+Route::get('/form/{surveyId}', [SurveyController::class, 'show'])->name('surveys.show');
 Route::post('/form/{surveyId}', [SurveyController::class, 'submit'])->name('surveys.submit');
 Route::get('/finish', [SurveyController::class, 'finish'])->name('finish');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [SurveyController::class, 'index'])->name('survey.index');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('surveys', SurveyController::class); // Automatically handles CRUD (index, create, store, show, etc.)
+    Route::get('/survey/add-question', [SurveyController::class, 'addQuestion'])->name('surveys.add-question');
+    Route::get('/survey/{surveyId}/add-question', [QuestionController::class, 'addQuestion'])->name('questions.add-question');
+    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
+    Route::get('/form/{surveyId}/statistic', [SurveyController::class, 'showStatistics'])->name('surveys.statistic');
+});
 require __DIR__ . '/auth.php';
